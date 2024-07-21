38b8bbd42e87be0c4083d429f699791e2730241a
MikeeBuilds <citizensofandrometta@gmail.com>
2024-07-20 21:08:55 -0400

    initial commit

--- .history/app/(tabs)/_layout_20240720205540.tsx
+++ .history/app/(tabs)/_layout_20240720205540.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Dashboard',
+          title: 'Home',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
@@ -32,7 +32,15 @@ export default function TabLayout() {
           ),
         }}
       />
-      
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
     </Tabs>
   );
 }
